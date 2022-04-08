cee7ac5d44484250583933ced849739d4fddab30
MichaelaMeadows <michael.meadows09@gmail.com>
2022-04-08 08:39:21 -0400

    Adding placeholders for new concepts

---
+++ Global.cs
@@ -0,0 +1,12 @@
+﻿using SpaceSimulation.Empires;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceSimulation
+{
+    static class Global
+    {
+        public static List<Empire> Empires { get; set; }
+    }
+}
---
+++ Nodes/Node.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using SpaceSimulation.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceSimulation.Nodes
+{
+    class Node : Entity
+    {
+        public int id { get; set; }
+        public String name { get; set; }
+        public int outputVolume { get; set; }
+        public Tuple<int, int> location { get; set; }
+
+        public Tuple<int, int> getLocation()
+        {
+            return location;
+        }
+
+        public int getSize()
+        {
+            return 1;
+        }
+
+        public string getSprite()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setLocation(Tuple<int, int> location)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
